77b2c1fdc972322b28a93313d2b36c911607681c
karol cardona <kfcardonaaponte14@gmail.com>
2023-11-14 02:45:52 -0500

    primer commit

---
+++ lista/src/Credenciales.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcZvR9f8JpZUgEWJHo1Cvy2n0B1tgDOio",
+  authDomain: "jc-2023-8b737.firebaseapp.com",
+  projectId: "jc-2023-8b737",
+  storageBucket: "jc-2023-8b737.appspot.com",
+  messagingSenderId: "52892259286",
+  appId: "1:52892259286:web:4599718de5efe6fe174a77"
+};
+
+// Initialize Firebase
+const appFirabase = initializeApp(firebaseConfig);
+export default appFirabase; 
